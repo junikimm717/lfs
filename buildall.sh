6a23b0b1aff97d70b655da05b80fe3a4d6695ba3
Juni Kim <junikimm717@gmail.com>
2025-06-05 05:31:11 +0900

    manually set all environment variables in buildall

--- buildall.sh
+++ buildall.sh
@@ -4,7 +4,22 @@ set -eu
 
 DIR="$(realpath "$(dirname "$0" )" )"
 
-eval "$("$DIR/tools/env.sh")"
+export PATH="$DIR/cross/$TARGET-native/bin:$PATH"
+export CC="$DIR/cross/$TARGET-native/bin/$TARGET-gcc"
+export CXX="$DIR/cross/$TARGET-native/bin/$TARGET-g++"
+export CPP="$TARGET-gcc -E"
+export CXXCPP="$TARGET-g++ -E"
+export AR="$DIR/cross/$TARGET-native/bin/$TARGET-ar"
+export RANLIB="$DIR/cross/$TARGET-native/bin/$TARGET-ranlib"
+export LD="$DIR/cross/$TARGET-native/bin/$TARGET-ld"
+export DIST="$DIR/dist"
+export ROOTFS="$DIR/dist/rootfs"
+export CFLAGS="$CFLAGS -O2 -pipe -fPIC -w -I$ROOTFS/usr/include -I$ROOTFS/include -I$DIR/cross/$TARGET-native/$TARGET/include"
+export CXXFLAGS="$CXXFLAGS -O2 -pipe -fPIC -w -I$ROOTFS/usr/include -I$ROOTFS/include -I$DIR/cross/$TARGET-native/$TARGET/include"
+export LDFLAGS="$LDFLAGS -s -L$ROOTFS/lib -L$ROOTFS/usr/lib -L$ROOTFS/usr/lib64 -L$DIR/cross/$TARGET-native/$TARGET/lib"
+export PKG_CONFIG_SYSROOT_DIR=$ROOTFS
+export PKG_CONFIG_LIBDIR=$ROOTFS/usr/lib/pkgconfig
+export PKG_CONFIG_PATH=$ROOTFS/usr/share/pkgconfig
 
 "$DIR/core/kernel/build" all
 "$DIR/tools/setup_initramfs.sh"
