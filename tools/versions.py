8f6ae6941e898ff4cc74bc53c3188869369504c1
Juni Kim <junikimm717@gmail.com>
2025-08-20 07:01:03 -0400

    versions.py formatting

--- tools/versions.py
+++ tools/versions.py
@@ -8,18 +8,22 @@ from dataclasses import dataclass
 
 COREDIR = Path(__file__).parent.parent / "core"
 
+
 @dataclass
 class OutOfDate:
     name: str
     devmit: bool
     current: str
     expected: str
-    
-    def msg(self):
-        return f"{self.name} {'(installs from dev.mit.junic.kim!!) ' if self.devmit else ''}"\
-            f"is set to {self.current} (Expected {self.expected})"
 
-updates: 'list[OutOfDate]' = []
+    def msg(self):
+        return (
+            f"{self.name} {'(installs from dev.mit.junic.kim!!) ' if self.devmit else ''}"
+            f"is set to {self.current} (Expected {self.expected})"
+        )
+
+
+updates: "list[OutOfDate]" = []
 version_scripts = list(COREDIR.glob("*/version"))
 
 for vscript in version_scripts:
@@ -27,10 +31,14 @@ for vscript in version_scripts:
     pkg = vscript.parent.name
     sys.stderr.write(f"Checking that {pkg} is up-to-date...")
     sys.stderr.flush()
-    res = subprocess.run([vscript], capture_output=True, text=True, encoding="utf-8", check=False)
+    res = subprocess.run(
+        [vscript], capture_output=True, text=True, encoding="utf-8", check=False
+    )
     if res.returncode != 0:
         sys.stderr.write("\033[1;33mINCOMPLETE\033[0m ")
-        sys.stderr.write(f"version script invoked at {vscript} encountered an exit code of {res.returncode}")
+        sys.stderr.write(
+            f"version script invoked at {vscript} encountered an exit code of {res.returncode}"
+        )
         sys.stderr.write("\n")
         sys.stderr.flush()
         continue
@@ -38,14 +46,16 @@ for vscript in version_scripts:
     latest = res.stdout.strip()
     if not latest:
         sys.stderr.write("\033[1;33mINCOMPLETE\033[0m ")
-        sys.stderr.write(f"Invoked {vscript} has an empty output, skipping: {res.stderr}")
+        sys.stderr.write(
+            f"Invoked {vscript} has an empty output, skipping: {res.stderr}"
+        )
         sys.stderr.write("\n")
         sys.stderr.flush()
         continue
 
     # Try to extract a VERSION="xyz" declaration from the build script.
     build = (vscript.parent / "build").read_text(encoding="utf-8")
-    version = re.search(r'VERSION\=\"(.*)\"', build)
+    version = re.search(r"VERSION\=\"(.*)\"", build)
     if version is None:
         print(f"Package {pkg} has no registered version, skipping")
         continue
@@ -55,14 +65,11 @@ for vscript in version_scripts:
 
     # some packages download from Juni's mirror (bc gnu mirrors suck occasionally)
     # These should be highlighted, as there is no guarantee the tarballs will actually be there.
-    devmit = 'https://dev.mit.junic.kim' not in build
+    devmit = "https://dev.mit.junic.kim" not in build
     if latest != current:
-        updates.append(OutOfDate(
-            name=pkg,
-            devmit=devmit,
-            current=current,
-            expected=latest
-        ))
+        updates.append(
+            OutOfDate(name=pkg, devmit=devmit, current=current, expected=latest)
+        )
         sys.stderr.write("\033[1;31mNo\033[0m\n")
         sys.stderr.flush()
     else:
@@ -70,8 +77,10 @@ for vscript in version_scripts:
         sys.stderr.flush()
 
 if updates:
-    print(f"{len(updates)}/{len(version_scripts)} packages seem out-of-date. "
-        "Please update the $VERSION variable as specified in each package's ./build script.")
+    print(
+        f"{len(updates)}/{len(version_scripts)} packages seem out-of-date. "
+        "Please update the $VERSION variable as specified in each package's ./build script."
+    )
     for ood in updates:
         print(ood.msg())
     sys.exit(1)
